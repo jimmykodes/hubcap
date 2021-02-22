935f8a09d6ed26b3b32e1d1028a015d1ded4a549
jimmykodes <jkeith@kochava.com>
2021-02-22 15:46:14 -0800

    expand user dao

--- internal/dao/user.go
+++ internal/dao/user.go
@@ -16,21 +16,43 @@ type User interface {
 	Close() error
 }
 
+const (
+	createUser stmt = iota
+	getUser
+	updateUser
+	deleteUser
+)
+
 type user struct {
 	db    *sqlx.DB
 	stmts statements
 }
 
 func newUser(db *sqlx.DB) (*user, error) {
-	return &user{db: db}, nil
+	queries := map[stmt]string{
+		createUser: "INSERT INTO vehicles.users (email, api_key, super_user) value (?, ?, ?);",
+		getUser:    "SELECT id, email, api_key, super_user FROM vehicles.users WHERE id = ?;",
+		updateUser: "UPDATE vehicles.users SET email = ?, super_user = ? WHERE id = ?;",
+		deleteUser: "DELETE FROM vehicles.users WHERE id = ?",
+	}
+	s, err := prepareStatements(db, queries)
+	if err != nil {
+		return nil, err
+	}
+	return &user{db: db, stmts: s}, nil
 }
 
 func (u *user) Create(ctx context.Context, user *dto.User) error {
-	panic("implement me")
+	_, err := u.stmts[createUser].ExecContext(ctx, user)
+	return err
 }
 
 func (u *user) Get(ctx context.Context, id int64) (*dto.User, error) {
-	panic("implement me")
+	user := &dto.User{}
+	if err := u.stmts[getUser].GetContext(ctx, user, id); err != nil {
+		return nil, err
+	}
+	return user, nil
 }
 
 func (u *user) Select(ctx context.Context, sf SearchFilters) ([]*dto.User, error) {
@@ -38,11 +60,13 @@ func (u *user) Select(ctx context.Context, sf SearchFilters) ([]*dto.User, error
 }
 
 func (u *user) Update(ctx context.Context, user *dto.User) error {
-	panic("implement me")
+	_, err := u.stmts[updateUser].ExecContext(ctx, user)
+	return err
 }
 
 func (u *user) Delete(ctx context.Context, id int64) error {
-	panic("implement me")
+	_, err := u.stmts[deleteUser].ExecContext(ctx, id)
+	return err
 }
 
 func (u *user) Close() error {
